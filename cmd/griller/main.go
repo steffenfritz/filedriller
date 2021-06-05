38b691833a79f55af99332afd06cf9e84f570620
dla-fritz <steffen.fritz@dla-marbach.de>
2021-06-05 14:26:39 +0200

    working copy

--- cmd/griller/main.go
+++ cmd/griller/main.go
@@ -1,4 +1,26 @@
 package main
 
+import (
+	"log"
+	"os"
+
+	"fyne.io/fyne"
+	"fyne.io/fyne/app"
+	//"fyne.io/fyne/canvas"
+	"fyne.io/fyne/layout"
+	"fyne.io/fyne/widget"
+
+	"github.com/dla-marbach/filedriller"
+)
+
 func main(){
+	a := app.New()
+	w := a.NewWindow("filedriller - GUI")
+
+	// Toolbar
+	toolbar := createToolbar() // ToDo
+
+	// Progress bar
+	infinite := widget.NewProgressBarInfinite()
+	infinite.Hidden = true
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/dla-marbach/filedriller
 go 1.15
 
 require (
+	fyne.io/fyne v1.4.3
 	github.com/gomodule/redigo v1.8.3
 	github.com/google/uuid v1.1.4
 	github.com/richardlehane/siegfried v1.9.1
